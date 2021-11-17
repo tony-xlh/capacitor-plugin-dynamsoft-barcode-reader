0b7a6bf33e470dce0169e6b3796f208ab1d22cdb
xulihang <xulihanghai@163.com>
2021-11-17 16:31:17 +0800

    add camera view as a sibling view of webview instead of using an activity

--- android/src/main/java/com/dynamsoft/dbr/DBRPlugin.java
+++ android/src/main/java/com/dynamsoft/dbr/DBRPlugin.java
@@ -1,67 +1,153 @@
 package com.dynamsoft.dbr;
 
-import android.content.Intent;
+import android.graphics.Color;
 import android.util.Base64;
-import android.widget.Toast;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.FrameLayout;
 
-import androidx.activity.result.ActivityResult;
-import androidx.activity.result.ActivityResultCallback;
-import androidx.activity.result.ActivityResultLauncher;
-import androidx.activity.result.contract.ActivityResultContracts;
-
+import com.dynamsoft.dce.CameraEnhancer;
+import com.dynamsoft.dce.DCECameraView;
+import com.dynamsoft.dce.DCELicenseVerificationListener;
 import com.getcapacitor.JSObject;
 import com.getcapacitor.Plugin;
 import com.getcapacitor.PluginCall;
 import com.getcapacitor.PluginMethod;
-import com.getcapacitor.annotation.ActivityCallback;
 import com.getcapacitor.annotation.CapacitorPlugin;
 
-import java.util.ArrayList;
-import java.util.List;
 
 @CapacitorPlugin(name = "DBR")
 public class DBRPlugin extends Plugin {
 
     private DBR implementation = new DBR();
-    private final int RequestCode_Scan = 0;
-
+    private CameraEnhancer mCameraEnhancer = null;
+    private DCECameraView mCameraView;
+    private BarcodeReader reader = null;
+    private String currentCallbackID;
     @PluginMethod
-    public void echo(PluginCall call) {
-        String value = call.getString("value");
-
-        JSObject ret = new JSObject();
-        ret.put("value", implementation.echo(value));
-        call.resolve(ret);
+    public void scan(PluginCall call) {
+        String dceLicense = "DLS2eyJvcmdhbml6YXRpb25JRCI6IjIwMDAwMSJ9";
+        String organizationID = "200001";
+        String license = "";
+        dceLicense = call.getString("dceLicense",dceLicense);
+        organizationID = call.getString("organizationID",organizationID);
+        license = call.getString("license",license);
+        call.setKeepAlive(true);
+        currentCallbackID = call.getCallbackId();
+        init(license,organizationID,dceLicense);
     }
 
-    @PluginMethod
-    public void scan(PluginCall call) throws InterruptedException {
-        Intent intent = new Intent(getBridge().getActivity() , ScannerActivity.class);
-        if (call.hasOption("license")) {
-            intent.putExtra("license",call.getString("license"));
+    class InitThread implements Runnable{
+        String dcelicense;
+        String license;
+        String organizationID;
+        public InitThread(String license, String organizationID, String dcelicense) {
+            this.license = license;
+            this.organizationID = organizationID;
+            this.dcelicense = dcelicense;
         }
-        if (call.hasOption("organizationID")){
-            intent.putExtra("organizationID",call.getString("organizationID"));
+
+        @Override
+        public void run(){
+            boolean notInitialized = false;
+            if (reader == null){
+                notInitialized = true;
+            }
+            if (notInitialized){
+                initDBR(license,organizationID);
+                initDCE(dcelicense);
+                TextResultCallback mTextResultCallback = new TextResultCallback() {
+                    // Obtain the recognized barcode results and display.
+                    @Override
+                    public void textResultCallback(int i, TextResult[] textResults, Object userData) {
+                        System.out.println("Found "+textResults.length+" barcode(s).");
+                        if (textResults.length>0){
+                            PluginCall call = bridge.getSavedCall(currentCallbackID);
+                            try{
+                                reader.StopCameraEnhancer();
+                                mCameraView.setVisibility(View.INVISIBLE);
+                                TextResult result = textResults[0];
+                                JSObject ret = new JSObject();
+                                ret.put("barcodeText", result.barcodeText);
+                                ret.put("barcodeFormat", result.barcodeFormatString);
+                                ret.put("barcodeBytesBase64", Base64.encodeToString(result.barcodeBytes,Base64.DEFAULT));
+                                call.resolve(ret);
+                            }catch (Exception e){
+                                call.reject(e.getMessage());
+                            }
+                        }
+                    }
+                };
+
+                DCESettingParameters dceSettingParameters = new DCESettingParameters();
+                dceSettingParameters.cameraInstance = mCameraEnhancer;
+                dceSettingParameters.textResultCallback = mTextResultCallback;
+                reader.SetCameraEnhancerParam(dceSettingParameters);
+            }else{
+                mCameraView.setVisibility(View.VISIBLE);
+            }
+            reader.StartCameraEnhancer();
         }
-        if (call.hasOption("dceLicense")){
-            intent.putExtra("dceLicense",call.getString("dceLicense"));
-        }
-        startActivityForResult(call,intent,"onScanned");
     }
 
-    @ActivityCallback
-    private void onScanned(PluginCall call, ActivityResult result) {
-        if (call == null) {
-            return;
+
+    private void init(String license, String organizationID,String dcelicense){
+        InitThread t = new InitThread(license,organizationID,dcelicense);
+        getActivity().runOnUiThread(t);
+    }
+
+    private void initDBR(String license, String organizationID){
+        try {
+            reader = new BarcodeReader();
+        } catch (BarcodeReaderException e) {
+            e.printStackTrace();
         }
-        try{
-            JSObject ret = new JSObject();
-            ret.put("barcodeText", result.getData().getStringExtra("barcodeText"));
-            ret.put("barcodeFormat", result.getData().getStringExtra("barcodeFormat"));
-            ret.put("barcodeBytesBase64", result.getData().getStringExtra("barcodeBytesBase64"));
-            call.resolve(ret);
-        }catch (Exception e){
-            call.reject(e.getMessage());
+
+        DMDLSConnectionParameters dbrParameters = new DMDLSConnectionParameters();
+        if (!license.equals("")){
+            try {
+                reader.initLicense(license);
+            } catch (BarcodeReaderException e) {
+                e.printStackTrace();
+            }
+        }else{
+            dbrParameters.organizationID = organizationID;
+            reader.initLicenseFromDLS(dbrParameters, new DBRDLSLicenseVerificationListener() {
+                @Override
+                public void DLSLicenseVerificationCallback(boolean isSuccessful, Exception e) {
+                    if (!isSuccessful) {
+                        e.printStackTrace();
+                    }
+                }
+            });
         }
+    }
+
+    private void initDCE(String dceLicense){
+        CameraEnhancer.initLicense(dceLicense, new DCELicenseVerificationListener() {
+            @Override
+            public void DCELicenseVerificationCallback(boolean isSuccess, Exception error) {
+                if(!isSuccess){
+                    error.printStackTrace();
+                }
+            }
+        });
+
+        mCameraEnhancer = new CameraEnhancer(getActivity());
+        mCameraView = new DCECameraView(getActivity());
+        mCameraEnhancer.setCameraView(mCameraView);
+        mCameraView.setOverlayVisible(true);
+        FrameLayout.LayoutParams cameraPreviewParams = new FrameLayout.LayoutParams(
+                FrameLayout.LayoutParams.WRAP_CONTENT,
+                FrameLayout.LayoutParams.WRAP_CONTENT
+        );
+        ((ViewGroup) bridge.getWebView().getParent()).addView(mCameraView,cameraPreviewParams);
+        bridge.getWebView().bringToFront();
+        makeWebViewTransparent();
+    }
+
+    private void makeWebViewTransparent(){
+        bridge.getWebView().setBackgroundColor(Color.TRANSPARENT);
+        //bridge.getWebView().evaluateJavascript("document.body.style.display='none'",null);
     }
 }
--- android/src/main/java/com/dynamsoft/dbr/ScannerActivity.java
+++ android/src/main/java/com/dynamsoft/dbr/ScannerActivity.java
@@ -9,9 +9,7 @@ import android.os.Bundle;
 import android.util.Base64;
 
 public class ScannerActivity extends AppCompatActivity {
-    private CameraEnhancer mCameraEnhancer;
-    private DCECameraView mCameraView;
-    private BarcodeReader reader;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -32,73 +30,13 @@ public class ScannerActivity extends AppCompatActivity {
         System.out.println(dceLicense);
         System.out.println(license);
         System.out.println(organizationID);
-        CameraEnhancer.initLicense(dceLicense, new DCELicenseVerificationListener() {
-            @Override
-            public void DCELicenseVerificationCallback(boolean isSuccess, Exception error) {
-                if(!isSuccess){
-                    error.printStackTrace();
-                }
-            }
-        });
 
 
-        mCameraEnhancer = new CameraEnhancer(ScannerActivity.this);
 
 
-        mCameraView = findViewById(R.id.cameraView);
-        mCameraEnhancer.setCameraView(mCameraView);
-        mCameraView.setOverlayVisible(true);
-
-        try {
-            reader = new BarcodeReader();
-        } catch (BarcodeReaderException e) {
-            e.printStackTrace();
-        }
-
-        DMDLSConnectionParameters dbrParameters = new DMDLSConnectionParameters();
-        if (license!=""){
-            try {
-                reader.initLicense(license);
-            } catch (BarcodeReaderException e) {
-                e.printStackTrace();
-            }
-        }else{
-            dbrParameters.organizationID = organizationID;
-            reader.initLicenseFromDLS(dbrParameters, new DBRDLSLicenseVerificationListener() {
-                @Override
-                public void DLSLicenseVerificationCallback(boolean isSuccessful, Exception e) {
-                    if (!isSuccessful) {
-                        e.printStackTrace();
-                    }
-                }
-            });
-        }
 
 
-        TextResultCallback mTextResultCallback = new TextResultCallback() {
-            // Obtain the recognized barcode results and display.
-            @Override
-            public void textResultCallback(int i, TextResult[] textResults, Object userData) {
-                System.out.println("Found "+textResults.length+" barcode(s).");
-                if (textResults.length>0){
-                    TextResult result = textResults[0];
-                    Intent intent = new Intent();
-                    intent.putExtra("barcodeText", result.barcodeText);
-                    intent.putExtra("barcodeFormat", result.barcodeFormatString);
-                    intent.putExtra("barcodeBytesBase64", Base64.encodeToString(result.barcodeBytes,Base64.DEFAULT));
-                    setResult(0, intent);
-                    reader.StopCameraEnhancer();
-                    reader.destroy();
-                    finish();
-                }
-            }
-        };
 
-        DCESettingParameters dceSettingParameters = new DCESettingParameters();
-        dceSettingParameters.cameraInstance = mCameraEnhancer;
-        dceSettingParameters.textResultCallback = mTextResultCallback;
-        reader.SetCameraEnhancerParam(dceSettingParameters);
-        reader.StartCameraEnhancer();
     }
 
     @Override
@@ -114,4 +52,5 @@ public class ScannerActivity extends AppCompatActivity {
         // Stop video barcode reading
         reader.StopCameraEnhancer();
     }
+
 }

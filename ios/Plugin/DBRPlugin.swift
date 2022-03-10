43b38de482c4a8289e3790a24025bb7268ad7b77
xulihang <xulihanghai@163.com>
2022-03-10 01:37:59 -0800

    iOS: correctly rotate frame

--- ios/Plugin/DBRPlugin.swift
+++ ios/Plugin/DBRPlugin.swift
@@ -161,8 +161,6 @@ public class DBRPlugin: CAPPlugin, DMDLSLicenseVerificationDelegate, DCEFrameLis
         
         let rotatedImage = rotated(degrees:CGFloat(frame.orientation), image: image) ?? image
 
-        print(rotatedImage.size.width)
-        print(rotatedImage.size.height)
         let results = try? barcodeReader.decode(rotatedImage, withTemplate: "");
         let count = results?.count ?? 0
         NSLog("Found %d barcode(s)", count)
@@ -192,46 +190,41 @@ public class DBRPlugin: CAPPlugin, DMDLSLicenseVerificationDelegate, DCEFrameLis
     }
     
     func rotated(degrees: CGFloat, image: UIImage) -> UIImage? {
-        
-        let degreesToRadians: (CGFloat) -> CGFloat = { (degrees: CGFloat) in
-          return degrees / 180.0 * CGFloat.pi
+        var correctedDegrees:CGFloat = 0;
+        if degrees == 90 {
+            correctedDegrees = 270
+        } else if degrees == 180 {
+            correctedDegrees = 0
+        } else if degrees == 0 {
+            correctedDegrees = 180
         }
         
-        // Calculate the size of the rotated view's containing box for our drawing space
-        let rotatedViewBox: UIView = UIView(frame: CGRect(origin: .zero, size: image.size))
-        rotatedViewBox.transform = CGAffineTransform(rotationAngle: degreesToRadians(degrees))
-        let rotatedSize: CGSize = rotatedViewBox.frame.size
-
-        // Create the bitmap context
-        UIGraphicsBeginImageContextWithOptions(rotatedSize, false, 0.0)
-
-        guard let bitmap: CGContext = UIGraphicsGetCurrentContext(), let unwrappedCgImage: CGImage = image.cgImage else {
-          return nil
+        if correctedDegrees == 0 {
+            return image
         }
-
-        // Move the origin to the middle of the image so we will rotate and scale around the center.
-        bitmap.translateBy(x: rotatedSize.width/2.0, y: rotatedSize.height/2.0)
-
-        // Rotate the image context
-        bitmap.rotate(by: degreesToRadians(degrees))
-
-        bitmap.scaleBy(x: CGFloat(1.0), y: -1.0)
-
-        let rect: CGRect = CGRect(
-            x: -image.size.width/2,
-            y: -image.size.height/2,
-            width: image.size.width,
-            height: image.size.height)
-
-        bitmap.draw(unwrappedCgImage, in: rect)
-
-        guard let newImage: UIImage = UIGraphicsGetImageFromCurrentImageContext() else {
-          return nil
-        }
-
-        UIGraphicsEndImageContext()
-
-        return newImage
+        
+        let radians = correctedDegrees * .pi / 180
+        let cgImage = image.cgImage!
+        let LARGEST_SIZE = CGFloat(max(image.size.width, image.size.height))
+        let context = CGContext.init(data: nil, width:Int(LARGEST_SIZE), height:Int(LARGEST_SIZE), bitsPerComponent: cgImage.bitsPerComponent, bytesPerRow: 0, space: cgImage.colorSpace!, bitmapInfo: cgImage.bitmapInfo.rawValue)!
+       
+        var drawRect = CGRect.zero
+        drawRect.size = image.size
+        let drawOrigin = CGPoint(x: (LARGEST_SIZE - image.size.width) * 0.5,y: (LARGEST_SIZE - image.size.height) * 0.5)
+        drawRect.origin = drawOrigin
+        var tf = CGAffineTransform.identity
+        tf = tf.translatedBy(x: LARGEST_SIZE * 0.5, y: LARGEST_SIZE * 0.5)
+        tf = tf.rotated(by: CGFloat(radians))
+        tf = tf.translatedBy(x: LARGEST_SIZE * -0.5, y: LARGEST_SIZE * -0.5)
+        context.concatenate(tf)
+        context.draw(cgImage, in: drawRect)
+        var rotatedImage = context.makeImage()!
+       
+        drawRect = drawRect.applying(tf)
+       
+        rotatedImage = rotatedImage.cropping(to: drawRect)!
+        let resultImage = UIImage(cgImage: rotatedImage)
+        return resultImage
       }
 
     public func dlsLicenseVerificationCallback(_ isSuccess: Bool, error: Error?) {

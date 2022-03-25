4bcc67a69c6e378849bf6b3469423ec6bcfd9a37
xulihang <xulihanghai@163.com>
2022-03-25 14:00:32 +0800

    JS: update to 9.0 with DCE 2.3.1

--- src/web.ts
+++ src/web.ts
@@ -1,18 +1,25 @@
 import { WebPlugin } from '@capacitor/core';
 
 import { DBRPlugin, EnumResolution, Options, ScanRegion, ScanResult, TextResult } from './definitions';
-import DBR, { BarcodeScanner, TextResult as DBRTextResult } from "dynamsoft-javascript-barcode";
+import { BarcodeReader, BarcodeScanner, TextResult as DBRTextResult } from "dynamsoft-javascript-barcode";
+import { CameraEnhancer } from 'dynamsoft-camera-enhancer';
+import { PlayCallbackInfo } from 'dynamsoft-camera-enhancer/dist/types/interface/playcallbackinfo';
 
-DBR.engineResourcePath = "https://cdn.jsdelivr.net/npm/dynamsoft-javascript-barcode@8.8.7/dist/";
+BarcodeReader.engineResourcePath = "https://cdn.jsdelivr.net/npm/dynamsoft-javascript-barcode@9.0.0/dist/";
+CameraEnhancer.defaultUIElementURL = "https://cdn.jsdelivr.net/npm/dynamsoft-camera-enhancer@2.3.1/dist/dce.ui.html";
 
 export class DBRWeb extends WebPlugin implements DBRPlugin {
-  private scanner!: BarcodeScanner;
+  private reader!: BarcodeReader;
+  private enhancer!: CameraEnhancer;
+  private interval!: any;
+  private decoding: boolean = false;
+
   async toggleTorch(options:{ on:boolean}){
     try{
       if (options["on"]){
-        this.scanner.turnOnTorch();
+        this.enhancer.turnOnTorch();
       }else{
-        this.scanner.turnOffTorch();
+        this.enhancer.turnOffTorch();
       }
     } catch (e){
       throw new Error("Torch unsupported");
@@ -21,7 +28,10 @@ export class DBRWeb extends WebPlugin implements DBRPlugin {
 
   async stopScan(){
     try{
-      await this.scanner.hide();
+      if (this.interval) {
+        clearInterval(this.interval);
+      }
+      this.enhancer.close(true);
     } catch (e){
       throw e;
     }
@@ -29,7 +39,10 @@ export class DBRWeb extends WebPlugin implements DBRPlugin {
 
   async pauseScan(){
     try{
-      this.scanner.pauseScan();
+      if (this.interval) {
+        clearInterval(this.interval);
+      }
+      this.enhancer.pause();
     } catch (e){
       throw e;
     }
@@ -37,81 +50,116 @@ export class DBRWeb extends WebPlugin implements DBRPlugin {
 
   async resumeScan(){
     try{
-      this.scanner.resumeScan();
+      await this.enhancer.resume();
+      if (this.interval) {
+        clearInterval(this.interval);
+      }
+      this.interval = setInterval(this.captureAndDecode, 100);
     } catch (e){
       throw e;
     }
   }
 
   async destroy():Promise<void>{
+    this.enhancer.getUIElement().remove();
     return
   }
 
   async initialize(options?:Options): Promise<{success:boolean}> {
-    if (this.scanner === undefined){
+    if (this.reader === undefined){
       if (options) {
         if (options.license){
-          DBR.BarcodeScanner.productKeys = options.license;
+          BarcodeReader.license = options.license;
         }
       }
-      this.scanner = await DBR.BarcodeScanner.createInstance();
-      this.scanner.onPlayed = rsl => {
-        this.notifyListeners("onPlayed", {resolution:rsl.width+"x"+rsl.height})
-      };
-      this.scanner.onFrameRead = results => {
-        var textResults = [];
-        for (let index = 0; index < results.length; index++) {
-          let result:DBRTextResult = results[index];
-          var tr:TextResult;
-          tr =  {
-                  barcodeText:result.barcodeText,
-                  barcodeFormat:result.barcodeFormatString,
-                  barcodeBytesBase64:this.arrayBufferToBase64(result.barcodeBytes),
-                  x1:result.localizationResult.x1,
-                  y1:result.localizationResult.y1,
-                  x2:result.localizationResult.x2,
-                  y2:result.localizationResult.y2,
-                  x3:result.localizationResult.x3,
-                  y3:result.localizationResult.y3,
-                  x4:result.localizationResult.x4,
-                  y4:result.localizationResult.y4,
-                }
-          textResults.push(tr)
-        }
 
-        var ret:ScanResult = {"results":textResults};
-        this.notifyListeners("onFrameRead", ret);
-      };
-      this.scanner.getUIElement().getElementsByClassName("dce-btn-close")[0].remove();
-      //this.scanner.getUIElement().getElementsByClassName("dbrScanner-cvs-drawarea")[0].remove();
-      this.scanner.getUIElement().getElementsByClassName("dce-sel-camera")[0].remove();
-      this.scanner.getUIElement().getElementsByClassName("dce-sel-resolution")[0].remove();
+      this.enhancer = await CameraEnhancer.createInstance();
+      this.reader = await BarcodeScanner.createInstance();
+      this.reader.updateRuntimeSettings('balance');
+      this.enhancer.on("played", (playCallBackInfo:PlayCallbackInfo) => {
+        this.notifyListeners("onPlayed", {resolution:playCallBackInfo.width+"x"+playCallBackInfo.height});
+      });
+      this.enhancer.getUIElement().getElementsByClassName("dce-btn-close")[0].remove();
+      this.enhancer.getUIElement().getElementsByClassName("dce-sel-camera")[0].remove();
+      this.enhancer.getUIElement().getElementsByClassName("dce-sel-resolution")[0].remove();
+      this.enhancer.getUIElement().getElementsByClassName("dce-msg-poweredby")[0].remove();
     }else{
       console.log("Scanner already initialized.");
     }
     return {success:true};
   }
 
+  async captureAndDecode() {
+    if (this.enhancer == undefined) {
+      return
+    }
+    if (this.enhancer.isOpen() == false) {
+      return;
+    }
+    if (this.decoding == true) {
+      return;
+    }
+    let frame = this.enhancer.getFrame();
+    console.log(frame);
+    if (frame) {
+      this.decoding = true;
+      let results:DBRTextResult[] = await this.reader.decode(frame);
+      this.decoding = false;
+      let textResults = [];
+      for (let index = 0; index < results.length; index++) {
+        let result:DBRTextResult = results[index];
+        let tr:TextResult;
+        let sx:number = 0;
+        let sy:number = 0;
+        if (frame.isCropped == true) {
+          sx = frame.sx;
+          sy = frame.sy;
+        }
+        tr =  {
+                barcodeText:result.barcodeText,
+                barcodeFormat:result.barcodeFormatString,
+                barcodeBytesBase64:this.arrayBufferToBase64(result.barcodeBytes),
+                x1:result.localizationResult.x1 + sx,
+                y1:result.localizationResult.y1 + sy,
+                x2:result.localizationResult.x2 + sx,
+                y2:result.localizationResult.y2 + sy,
+                x3:result.localizationResult.x3 + sx,
+                y3:result.localizationResult.y3 + sy,
+                x4:result.localizationResult.x4 + sx,
+                y4:result.localizationResult.y4 + sy,
+              }
+        textResults.push(tr)
+      }
+  
+      var ret:ScanResult = {"results":textResults};
+      this.notifyListeners("onFrameRead", ret);
+    }
+  }
+
   async setScanRegion(region:ScanRegion) {
-    let settings = await this.scanner.getRuntimeSettings();
-    settings.region.regionLeft = region.left;
-    settings.region.regionRight = region.right;
-    settings.region.regionTop = region.top;
-    settings.region.regionBottom = region.bottom;
-    settings.region.regionMeasuredByPercentage = region.measuredByPercentage;
-    await this.scanner.updateRuntimeSettings(settings);
+    this.enhancer.setScanRegion({
+      regionLeft:region.left,
+      regionTop:region.top,
+      regionRight:region.right,
+      regionBottom:region.bottom,
+      regionMeasuredByPercentage: region.measuredByPercentage
+    })
     return {success:true};
   }
 
   async initRuntimeSettingsWithString(options: { template: string; }): Promise<void> {
     if (options.template){
-      await this.scanner.initRuntimeSettingsWithString(options.template);
+      await this.reader.initRuntimeSettingsWithString(options.template);
       console.log("Using template");
     }
   }
 
   async startScan(): Promise<void> {
-    await this.scanner.show();
+    await this.enhancer.open(true);
+    if (this.interval) {
+      clearInterval(this.interval);
+    }
+    this.interval = setInterval(this.captureAndDecode.bind(this),100);
   }
 
   private arrayBufferToBase64( buffer: number[] ):string {
@@ -125,7 +173,7 @@ export class DBRWeb extends WebPlugin implements DBRPlugin {
   }
 
   async getAllCameras(): Promise<{ cameras?: string[] | undefined; message?: string | undefined; }> {
-    let cameras = await this.scanner.getAllCameras();
+    let cameras = await this.enhancer.getAllCameras();
     let labels:string[] = [];
     cameras.forEach(camera => {
       labels.push(camera.label);
@@ -134,10 +182,10 @@ export class DBRWeb extends WebPlugin implements DBRPlugin {
   }
 
   async selectCamera(options: { cameraID: string; }): Promise<{ success?: boolean | undefined; message?: string | undefined; }> {
-    let cameras = await this.scanner.getAllCameras()
+    let cameras = await this.enhancer.getAllCameras()
     cameras.forEach(async camera => {
       if (camera.label == options.cameraID) {
-        await this.scanner.setCurrentCamera(camera);
+        await this.enhancer.selectCamera(camera);
         return {success:true};
       }
     });
@@ -145,12 +193,12 @@ export class DBRWeb extends WebPlugin implements DBRPlugin {
   }
   
   async getSelectedCamera(): Promise<{ selectedCamera?: string | undefined; message?: string | undefined; }> {
-    let cameraInfo = await this.scanner.getCurrentCamera();
+    let cameraInfo = this.enhancer.getSelectedCamera();
     return {"selectedCamera":cameraInfo?.label};
   }
 
   async getResolution(): Promise<{ resolution?: string | undefined; message?: string | undefined; }> {
-    let rsl = this.scanner.getResolution();
+    let rsl = this.enhancer.getResolution();
     let res:string = rsl[0] + "x" + rsl[1];
     return {resolution:res};
   }
@@ -177,12 +225,12 @@ export class DBRWeb extends WebPlugin implements DBRPlugin {
       height = 2160;
     }
 
-    await this.scanner.setResolution(width,height);
+    await this.enhancer.setResolution(width,height);
     return {success:true};
   }
 
   async setZoom(options: { factor: number; }): Promise<{ success?: boolean | undefined; message?: string | undefined; }> {
-    await this.scanner.setZoom(options.factor);
+    await this.enhancer.setZoom(options.factor);
     return {success:true};
   }
 
